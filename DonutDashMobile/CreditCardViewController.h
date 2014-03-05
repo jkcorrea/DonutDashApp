5146c4688dffab84be17ddaaa5ee8390022c05d6
Skylar Weaver <weaver.skylar@gmail.com>
2014-03-05 14:47:39 -0500

    Added CC page and text inputs

--- DonutDashMobile/CreditCardViewController.h
+++ DonutDashMobile/CreditCardViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DonutDashTitleLabel.h"
 
-@interface CreditCardViewController : UIViewController
+@interface CreditCardViewController : UIViewController <UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet DonutDashTitleLabel *textMainTitle;
+@property (strong, nonatomic) IBOutlet UILabel *textSubTitle;
+@property (strong, nonatomic) IBOutlet UITextField *textCardNumber;
+@property (strong, nonatomic) IBOutlet UITextField *textExpDate;
+@property (strong, nonatomic) IBOutlet UITextField *textSecurityCode;
+
 
 @end
